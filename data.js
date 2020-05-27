73f651d614dc26e963de7fc03006e4b8d32d22f6
thiagobsv <62785932+thiagobsv@users.noreply.github.com>
2020-05-26 23:56:05 -0500

    Update data.js

--- data.js
+++ data.js
@@ -1,9 +1,9 @@
 module.exports = {
 
   token: '1233615113:AAFkwb_9qT678Yik8zbGV0H5KEN5vtCLWoc',
-  mongoLink: 'mongodb+srv://chat:admin@cluster0-g4hlr.mongodb.net/iktzi?retryWrites=true&w=majority',
-  chats: [-1001381307542, -1001166073886, -1001410544997], // your chats
-  botId: 1075517277, // bot id
+  mongoLink: 'mongodb+srv://chat:Mongo2015@thiago-mvego.gcp.mongodb.net/test?retryWrites=true&w=majority',
+  chats: [-1001315976730, -1001166073886, -1001410544997], // your chats
+  botId: 1233615113, // bot id
   devId: 62253745,
 
   time: {
@@ -14,10 +14,10 @@ module.exports = {
   },
   
   timeNames: { // you can change lang.
-    m: 'мин.',
-    h: 'час.',
-    d: 'дн.',
-    w: 'нед.'
+    m: 'mes.',
+    h: 'hora',
+    d: 'dia',
+    w: 'semana'
   }
 
 }
